0a16dab7a3cbe32b783eb2501350c96acab12cec
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-24 16:37:38 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ group :development, :test do
   gem 'byebug'
 
   gem 'pry'
+  gem "bcrypt"
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+      def new 
+        @user = User.new
+      end
+      
+      def create 
+          raise.params.inspect 
+         @user = User.new 
+         @user = User.find(params[:user][:name])
+         @user = User.find(params[:user][:password])
+         if user.save
+         session[:user_id] = @user.id
+         redirect_to root_path
+         else
+         render '/users/new'
+         end
+      end
+  
+  
+end
