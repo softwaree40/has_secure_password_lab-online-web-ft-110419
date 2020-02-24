e5dfa81586aeb89e3da42c8b462dd84cdaeb7041
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-24 16:45:06 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,20 @@
 class UsersController < ApplicationController
-      def new 
-        @user = User.new
-      end
+  def new 
+   @user = User.new
+  end
       
-      def create 
-          
-         @user = User.new 
-         @user = User.find(params[:user][:name])
-         @user = User.find(params[:user][:password])
-         if user.save
-         session[:user_id] = @user.id
-         redirect_to root_path
-         else
-         render '/users/new'
-         end
-      end
+  def create 
+    @user = User.new 
+    @user = User.find(params[:user][:name])
+    @user = User.find(params[:user][:password])
+    if user.save
+      session[:user_id] = @user.id
+     redirect_to root_path
+     else
+      render '/users/new'
+     end    
+         
+  end
   
   
 end
